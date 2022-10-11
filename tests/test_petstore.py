4a02d28c64cd1f1471c6dc921b7d860de6a57a99
Kovalev <Kovalev.K@citilink.ru>
2022-10-11 10:09:58 +0300

    new tests python

--- tests/test_petstore.py
+++ tests/test_petstore.py
@@ -1,13 +1,25 @@
 import requests
 import pytest
 
+req = requests.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available")
+array = req.json()
+petId = ""
+petName = ""
+
+for pet in array:
+    if "name" in pet:
+        print(pet)
+        petId = pet['id']
+        petName = pet['name']
+        break
 
 
 def test_status_code():
-    response = requests.get("https://petstore.swagger.io/v2/pet/1")
+    response = requests.get("https://petstore.swagger.io/v2/pet/" + str(petId))
     assert response.status_code == 200
 
-def test_namepet():
-    response = requests.get("https://petstore.swagger.io/v2/pet/1")
-    print(response.json())
-    assert response.json()['name'] == 'doggie'
+
+def test_name_pet():
+    response = requests.get("https://petstore.swagger.io/v2/pet/" + str(petId))
+    print(response.json()['name'])
+    assert response.json()['name'] == petName
